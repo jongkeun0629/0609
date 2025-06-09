14c332b00c6c111d3cd1b15ece0649e3baa5cc6d
jongkeun0629 <dlwhdrms0629@gmail.com>
2025-06-09 14:54:22 +0900

    문자열 메서드2

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,5 @@
 public class Main {
     public static void main(String[] args){
-        String message = "Hello, Java!!";
-
-        System.out.println(message);
-
-        System.out.println(message.length());
-
-        System.out.println(message.toUpperCase());
-        System.out.println(message.toLowerCase());
-
-        System.out.println(message.indexOf("l"));
-
-        // indexOf() 로직 구현
-        for (int i = 0; i < message.length(); i++){
-            if (message.charAt(i) == 'l'){
-                System.out.println(i);
-                break;
-            }
-        }
-        System.out.println(message.substring(3,6));
-
         String address = "0x120819f5573ac3A530e6172b1581613A09b4b338";
 
         System.out.println(
