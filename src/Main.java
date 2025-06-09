1201211fd0e1f2d2395be411da6b21f178986cd4
jongkeun0629 <dlwhdrms0629@gmail.com>
2025-06-09 14:34:08 +0900

    문자열 메서드

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,31 @@
 public class Main {
     public static void main(String[] args){
-        int[] numbers = {2, 4, 6, 8, 10};
-        int sum = 0;
+        String message = "Hello, Java!!";
 
-        for (int i = 0; i < numbers.length; i++){
-            sum += numbers[i];
+        System.out.println(message);
+
+        System.out.println(message.length());
+
+        System.out.println(message.toUpperCase());
+        System.out.println(message.toLowerCase());
+
+        System.out.println(message.indexOf("l"));
+
+        // indexOf() 로직 구현
+        for (int i = 0; i < message.length(); i++){
+            if (message.charAt(i) == 'l'){
+                System.out.println(i);
+                break;
+            }
         }
+        System.out.println(message.substring(3,6));
 
-        System.out.println(sum);
+        String address = "0x120819f5573ac3A530e6172b1581613A09b4b338";
+
+        System.out.println(
+                address.substring(0, 7)
+                + "..."
+                + address.substring(address.length() - 5)
+        );
     }
 }
